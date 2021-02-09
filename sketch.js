db6de50af0a4894f60b2af6bcd2be5f9edebe292
teeeej17 <77414104+teeeej17@users.noreply.github.com>
2021-02-08 21:38:57 -0600

    sketch

--- sketch.js
+++ sketch.js
@@ -1,17 +1,8 @@
-const { STROKE } = require("./p5");
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    background(0, 0, 0)
   }
   
 function draw() {
-    STROKE(random(255), random(255), random(255));
-    circle(x, y, 100);
-    x+=random(-25, 25);
-    y+=random(-25, 25);
-    if(x>width) x=0;
-    else if(x<0) x=width;
-    if(y>height) y=0;
-    else if(y<height) y=0;
+  background(0, 0, 0)
+  circle(mouseX, mouseY, 50);
   }
