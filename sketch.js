35c49732cb559096bedb2b8ee88004f1357590cf
teeeej17 <77414104+teeeej17@users.noreply.github.com>
2021-02-08 22:35:45 -0600

    sketch

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const { FILL, _DEFAULT_FILL } = require("./p5");
+const { FILL, _DEFAULT_FILL, translator } = require("./p5");
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
@@ -8,10 +8,12 @@ function setup() {
 function draw() {
   const redVal = map(mouseX, 0, width, 0, 255);
   const greenVal = map(mouseY, 0, height, 0, 255);
-  noStroke();
-  circle(mouseX, mouseY, 50);
+  //noStroke();
+  //circle(mouseX, mouseY, 50);
 
-  
-  
-  
+  translate(mouseX, mouseY);
+  const rotation = map(mouseY, 0, height, 0, PI);
+  rotate(rotation);
+  stroke(redVal, greenVal, 255);
+  line(-50, 0, 50, 0); 
   }
