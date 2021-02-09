9510f454ed6669ab95a527864fa93011a6e70564
teeeej17 <77414104+teeeej17@users.noreply.github.com>
2021-02-08 22:07:01 -0600

    sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
+const { FILL, _DEFAULT_FILL } = require("./p5");
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    background(0, 0, 0)
   }
   
 function draw() {
-  background(0, 0, 0)
+  const redVal = map(mouseX, 0, width, 0, 255);
+  const greenVal = map(mouseY, 0, height, 0, 255);
+  noStroke();
   circle(mouseX, mouseY, 50);
+
+  
+  
+  
   }
