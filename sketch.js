6f7e5097c87d8d0569a6836f628ece7289ffc4e3
teeeej17 <77414104+teeeej17@users.noreply.github.com>
2021-02-08 22:57:47 -0600

    sketch

--- sketch.js
+++ sketch.js
@@ -1,35 +1,16 @@
-let x;
-let y;
-let speedX = 3;
-let speedY = 5;
-
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    x = random(0, width);
-    y = random(o, height);
-    speedX = random(-3, 3);
-    speedY = random(-3, 3);
     background(0, 0, 0)
   }
   
 function draw() {
-  const redVal = map(x, 0, width, 0, 255);
-  const greenVal = map(y, 0, height, 0, 255);
+  const redVal = map(mouseX, 0, width, 0, 255);
+  const greenVal = map(mouseY, 0, height, 0, 255);
 
-  translate(x, x);
-  const rotation = map(y, 0, height, 0, TWO_PI);
+  translate(mouseX, mouseY);
+  const rotation = map(mouseY, 0, height, 0, TWO_PI);
   rotate(rotation);
   stroke(redVal, greenVal, 255);
   line(-250, 0, 250, 0); 
 
-  x = x + speedX;
-  y = y + speedY;
-  if (x > width || x < 0) {
-    speedX = -speedX;
   }
-
-  if(y > height || y < 0) {
-    speedY = -speedY;
-  }
-}
